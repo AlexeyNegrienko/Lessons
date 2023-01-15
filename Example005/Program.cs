b6083c89f5e4e3eaeab5c41d748e89c2f99e9bd8
Алексей <kagan5@mail.ru>
2023-01-16 02:00:40 +0300

    Example005

---
+++ Example005/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("введите имя пользователя: ");
+string  username = Console.ReadLine();
+
+if (username.ToLower() == "маша")
+{
+    Console.WriteLine("Маша пришла");
+}
+else
+{
+  Console.Write("Здравствуй, ");
+  Console.WriteLine(username);  
+}
+
